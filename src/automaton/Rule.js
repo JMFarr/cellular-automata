a8946e02ba21508f70d1b35ec02b04532f182a0f
Jacob Farr <jacobmfarr@gmail.com>
2017-11-23 10:55:12 -0700

    added toggleable next state selector

--- src/automaton/Rule.js
+++ src/automaton/Rule.js
@@ -5,19 +5,29 @@ import './Rule.css'
 class Rule extends Component {
 	constructor({previousCellStates}) {
 		super()
-		this.previousCells = previousCellStates.map((state) => {
-			return <Cell state={state} />
-		})
+		this.previousCells = previousCellStates.map((state) => <Cell state={state} />)
 		this.state = {
 			nextCellState: 0
 		}
+
+		this.handleClick = this.handleClick.bind(this)
+		this.toggleNextCellState = this.toggleNextCellState.bind(this)		
+	}
+	handleClick(e) {
+		console.log('handleClick')
+		this.toggleNextCellState()
+	}
+	toggleNextCellState() {
+		this.setState({ 
+			nextCellState: !this.state.nextCellState
+		})
 	}
 	render() {
 		return (
 			<div className="rule">
 				{this.previousCells}
-				<div className='rule-toggle'>
-					<Cell state={this.nextCellState} />
+				<div className='rule-toggle' onClick={this.handleClick}>
+					<Cell state={this.state.nextCellState} />
 				</div>
 			</div>
 		)
