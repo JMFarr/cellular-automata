699fb77dab2305c41d55f2d84755af9789a5c40b
Jacob Farr <jacobmfarr@gmail.com>
2017-11-23 11:27:52 -0700

    toggleing rules now updates the ruleset

--- src/automaton/Rule.js
+++ src/automaton/Rule.js
@@ -3,24 +3,31 @@ import Cell from './Cell'
 import './Rule.css'
 
 class Rule extends Component {
-	constructor({previousCellStates}) {
+	constructor({previousCellStates, updateRuleset}) {
 		super()
+		this.previousCellStates = previousCellStates
 		this.previousCells = previousCellStates.map((state) => <Cell state={state} />)
+		this.updateRuleset = updateRuleset
 		this.state = {
 			nextCellState: 0
 		}
 
 		this.handleClick = this.handleClick.bind(this)
-		this.toggleNextCellState = this.toggleNextCellState.bind(this)		
+		this.toggleNextCellState = this.toggleNextCellState.bind(this)
+		this.getRule = this.getRule.bind(this)
 	}
 	handleClick(e) {
 		console.log('handleClick')
 		this.toggleNextCellState()
+		this.updateRuleset(this.getRule(), this.state.nextCellState)
 	}
 	toggleNextCellState() {
 		this.setState({ 
 			nextCellState: !this.state.nextCellState
 		})
+	}
+	getRule() {
+		return `${this.previousCellStates[0]}${this.previousCellStates[1]}${this.previousCellStates[2]}`
 	}
 	render() {
 		return (
--- src/automaton/RuleSelector.js
+++ src/automaton/RuleSelector.js
@@ -4,21 +4,27 @@ import Rule from './Rule'
 class RuleSelector extends Component {
 	constructor() {
 		super()
-		this.state = {
-
+		this.ruleset = []
+		this.updateRuleset = this.updateRuleset.bind(this)
+	}
+	updateRuleset(rule, shouldInclude) {
+		if(this.ruleset.indexOf(rule) > -1) {
+			this.ruleset.splice(this.ruleset.indexOf(rule), 1)
+		} else {
+			this.ruleset.push(rule)
 		}
 	}
 	render() {
 		return (
 			<div>
-				<Rule previousCellStates={[1,1,1]} />
-				<Rule previousCellStates={[1,1,0]} />
-				<Rule previousCellStates={[1,0,1]} />
-				<Rule previousCellStates={[1,0,0]} />
-				<Rule previousCellStates={[0,1,1]} />
-				<Rule previousCellStates={[0,1,0]} />
-				<Rule previousCellStates={[0,0,1]} />
-				<Rule previousCellStates={[0,0,0]} />
+				<Rule previousCellStates={[1,1,1]} updateRuleset={this.updateRuleset}/>
+				<Rule previousCellStates={[1,1,0]} updateRuleset={this.updateRuleset}/>
+				<Rule previousCellStates={[1,0,1]} updateRuleset={this.updateRuleset}/>
+				<Rule previousCellStates={[1,0,0]} updateRuleset={this.updateRuleset}/>
+				<Rule previousCellStates={[0,1,1]} updateRuleset={this.updateRuleset}/>
+				<Rule previousCellStates={[0,1,0]} updateRuleset={this.updateRuleset}/>
+				<Rule previousCellStates={[0,0,1]} updateRuleset={this.updateRuleset}/>
+				<Rule previousCellStates={[0,0,0]} updateRuleset={this.updateRuleset}/>
 			</div>
 		)
 	}
