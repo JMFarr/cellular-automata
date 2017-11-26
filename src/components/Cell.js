fe3ce7dc7949b9e9f7f969d9239ea054bb4ff750
Jacob Farr <jacobmfarr@gmail.com>
2017-11-25 17:43:59 -0700

    deleted all components to get a fresh start

--- src/components/Cell.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import './Cell.css'
-
-const Cell = ({state}) => {
-	const className = state ? 'cell active' : 'cell inactive'
-	return <div className={className}></div>
-}
-
-export default Cell
